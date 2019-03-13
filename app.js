fbcd4fbfdca5cda9a68ea36abdd8d667f80efb7b
Alex Waite <alexcwaite@gmail.com>
2019-03-13 18:05:51 -0400

    seperated app.js from server.js

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+const app = require('./server');
+const path = require('path');
 const db = require('./db');
+const { User, Thing, Favorite } = db.models;
 
-const { app } = require('./server');
+app.get('/app.js', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'dist', 'main.js'))
+);
+
+app.get('/', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+app.get('/users', (req, res, next) => {
+  User.findAll({
+    include: [
+      {
+        model: Favorite,
+        include: [{ model: Thing }],
+      },
+    ],
+  })
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => console.error(error));
+});
+
+app.get('/things', (req, res, next) => {
+  Thing.findAll({
+    include: [
+      {
+        model: Favorite,
+        include: [{ model: User }],
+      },
+    ],
+  })
+    .then(things => {
+      res.json(things);
+    })
+    .catch(error => console.error(error));
+});
--- server.js
+++ server.js
@@ -1,57 +1,14 @@
 const express = require('express');
 const app = express();
 const volleyball = require('volleyball');
-const path = require('path');
 const db = require('./db');
-const { User, Thing, Favorite } = db.models;
 
 const port = process.env.PORT || 3000;
 
 app.use(volleyball);
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
 db.syncAndSeed().then(() => {
   app.listen(port, () => console.log(`listening on port ${port}`));
-});
-
-app.get('/app.js', (req, res, next) =>
-  res.sendFile(path.join(__dirname, 'dist', 'main.js'))
-);
-
-app.get('/', (req, res, next) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
-);
-
-app.get('/users', (req, res, next) => {
-  User.findAll({
-    include: [
-      {
-        model: Favorite,
-        include: [{ model: Thing }],
-      },
-    ],
-  })
-    .then(users => {
-      res.json(users);
-    })
-    .catch(error => console.error(error));
-});
-
-app.get('/things', (req, res, next) => {
-  Thing.findAll({
-    include: [
-      {
-        model: Favorite,
-        include: [{ model: User }],
-      },
-    ],
-  })
-    .then(things => {
-      res.json(things);
-    })
-    .catch(error => console.error(error));
 });
 
 module.exports = app;
