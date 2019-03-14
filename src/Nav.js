8e007c5e2e41d4e3cfb1a7f33619055dd0de0067
Alex Waite <alexcwaite@gmail.com>
2019-03-13 22:05:54 -0400

    updated start scripts

--- src/Nav.js
+++ src/Nav.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+  function isActive() {
+    return location.href.includes('/users');
+  }
+
   return (
     <ul className="nav nav-pills">
       <li className="nav-item">
-        <Link className="nav-link" to="/users">
+        <Link
+          // className="nav-link"
+          className={
+            location.href.includes('/users') ? 'nav-link active' : 'nav-link'
+          }
+          to="/users"
+        >
           users
         </Link>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/things">
+        <Link
+          className={
+            location.href.includes('/things') ? 'nav-link active' : 'nav-link'
+          }
+          to="/things"
+        >
           things
         </Link>
       </li>
